26f1f5f0f638e399bf4bdbc1fc674d4ac356a9eb
Eldhose-Peter <eldhosepeter2000@gmail.com>
2024-10-31 00:04:54 +0530

    added nextJs pages

---
+++ client/src/components/layout.tsx
@@ -0,0 +1,18 @@
+
+import Footer from './footer'
+import Navbar from './navbar'
+import { ReactNode } from 'react'
+
+interface LayoutProps {
+  children: ReactNode
+}
+ 
+export default function Layout({ children }:LayoutProps) {
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+      <Footer />
+    </>
+  )
+}
---
+++ client/src/pages/about.tsx
@@ -0,0 +1,5 @@
+import Layout from "@/components/layout";
+
+export default function About() {
+    return <Layout><h1>This is about page</h1></Layout>
+  }
